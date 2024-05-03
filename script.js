1d3266065e7288af9c62e757b912efb228c764c2
Kavishka Chathumal <137688716+Kavishka119@users.noreply.github.com>
2024-05-03 16:30:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeView() {
+
+        var sd = document.getElementById("sd");
+        var hd = document.getElementById("hd");
+
+        sd.classList.toggle("d-none");
+        hd.classList.toggle("d-none");
+}
